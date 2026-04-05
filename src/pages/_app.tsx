febab47dc68325cd9a55958145c94627c2719a54
Justin Zhao <jzhaojy@gmail.com>
2026-04-04 21:50:26 -0400

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from "next/app";
 import "@/styles/index.scss";
 import "@fontsource/jetbrains-mono";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>;
 }
